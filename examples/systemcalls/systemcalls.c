fda6fc4f4302dbe4b20da3677238dfde6b7f40a7
ubuntu-vm <ubuntu-vm@cu-aeld-ubuntu-20.04.myguest.virtualbox.org>
2023-09-17 10:58:06 -0700

    Working commit of assignment 3 part 1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,11 @@
 #include "systemcalls.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <libgen.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -16,6 +23,12 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
+	int ret=-1;
+
+	ret = system(cmd);
+	if (ret < 0) {
+		return false;
+	}
 
     return true;
 }
@@ -47,8 +60,12 @@ bool do_exec(int count, ...)
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
 
+	if (command[0][0] != '/') {
+		printf("not absolute\n");
+		return false;
+	}
+	
 /*
  * TODO:
  *   Execute a system command by calling fork, execv(),
@@ -59,6 +76,27 @@ bool do_exec(int count, ...)
  *
 */
 
+	pid_t pid;
+	int status;
+	
+	pid = fork();
+
+	if (pid == -1) {
+		return false;
+	} else if (pid == 0) { // child
+		execv(command[0], command);		
+	} else { // parent
+	if (waitpid(pid, &status, 0) == -1) {
+		return false;
+	} else if (WIFEXITED(status)) {
+		printf("WIFEXITED %d", WEXITSTATUS(status));
+		if (WEXITSTATUS(status) == EXIT_SUCCESS) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	}
     va_end(args);
 
     return true;
@@ -82,7 +120,6 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
 
 
 /*
@@ -92,6 +129,26 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+
+	int kidpid;
+	int fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+	if (fd < 0) { perror("could not open outputfile"); abort(); }
+	switch (kidpid = fork()) {
+	    case -1: return false;
+	    case 0: // child code
+	        if (dup2(fd, 1) < 0) { perror("dup2"); abort(); }
+	        execv(command[0], command);// perror("execv"); abort();
+	        close(fd);
+			return false;
+	    default: // parent code
+	        close(fd);
+	}
+	int status;
+	if (wait(&status) == -1) {
+		return false;
+	} else if (WIFEXITED(status)) {
+		return WEXITSTATUS(status);
+	}
 
     va_end(args);
 
